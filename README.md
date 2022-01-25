85f25038ade430206274a41d508889cb6d62ca76
paulsukow <21179028+paulsukow@users.noreply.github.com>
2022-01-25 12:54:04 -0700

    parse CSV and add meals endpoint

---
+++ README.md
@@ -0,0 +1,7 @@
+Starting the backend
+
+```
+cd backend
+npm install
+node app.js
+```
---
+++ backend/app.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const csv = require('csv-parser')
+const fs = require('fs')
+
+const app = express()
+const port = 3000
+
+app.get('/meals', (req, res) => {
+  const meals = []
+  const filePath = './data/meals.csv'
+  fs.createReadStream(filePath)
+    .pipe(csv())
+    .on('data', (data) => meals.push(data))
+    .on('end', () => {
+      // console.log(meals)
+      res.send(meals)
+    })
+})
+
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`)
+})
